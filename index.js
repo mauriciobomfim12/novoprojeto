a31347254fc7edd5582de31a7b4016b857fe2cbf
mauriciobomfim12 <mauriciobomfimm@gmail.com>
2024-01-23 18:07:36 -0300

    commit3

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("Olá mundo");
     res.end();
-}).listen(3030, "0.0.0.0", () => console.log("Servidor rodando na porta 3030"));
+}).listen(80, "0.0.0.0", () => console.log("Servidor rodando na porta 80"));
